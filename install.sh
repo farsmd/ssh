00e6af0b3ecc8308d262b984c04d61b7687b7f7a
farshad <farsmd@gmail.com>
2024-06-03 19:44:38 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 # URL to the script on GitHub
-SCRIPT_URL="https://raw.githubusercontent.com/farsmd/ssh/main/ssh-user-manager.sh"
+SCRIPT_URL="https://raw.githubusercontent.com/farsmd/ssh/main/user_management.sh"
 
 # Download the script
 curl -O $SCRIPT_URL
 
 # Make the script executable
+sudo mv user_management.sh /usr/local/bin/ums
 chmod +x user_management.sh
 
 # Run the script
