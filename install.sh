ed814754af7c580d480f0fecc7b68bef32bda65a
farshad <farsmd@gmail.com>
2024-06-04 14:38:55 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # URL to the script on GitHub
-SCRIPT_URL="https://raw.githubusercontent.com/farsmd/ssh/main/user_management.sh"
-
+SCRIPT_URL="https://raw.githubusercontent.com/farsmd/ssh/main/app-ums.sh"
+SCRIPT_URL2="https://raw.githubusercontent.com/farsmd/ssh/main/required.sh"
 # Download the script
 curl -O $SCRIPT_URL
-
+curl -O $SCRIPT_URL2
+chmod +x required.sh
 # Make the script executable
-sudo mv user_management.sh /usr/local/bin/ums
-chmod +x user_management.sh
+sudo mv app-ums.sh /usr/local/bin/ums
+chmod +x app-ums.sh
 
-# Run the script
-./user_management.sh
+echo "Installation completed. Use 'ums' to manage users."
